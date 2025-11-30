430dbe866be3186c66487b27450c53ad6cddd41e
francoroguez <118698321+roguez26@users.noreply.github.com>
2025-11-29 19:54:13 -0600

    feat: implement review game

---
+++ lib/features/review_management/models/review_game_request.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/cupertino.dart';
+
+@immutable
+class ReviewGameRequest {
+
+  final int idGame;
+  final int idPlayer;
+  final double rating;
+  final String opinion;
+
+  const ReviewGameRequest({
+    required this.idGame,
+    required this.idPlayer,
+    required this.rating,
+    required this.opinion
+  });
+
+  Map<String, dynamic> toJson() => {
+
+    'idJuego': idGame,
+    'idJugador': idPlayer,
+    'calificacion': rating,
+    'comentario': opinion,
+  };
+}
