36c27de904b80102e27a5d994844aade08f3a875
francoroguez <118698321+roguez26@users.noreply.github.com>
2025-12-17 21:35:58 -0600

    feat: implement retrieve my review history

--- lib/features/review_management/views/review_history_screen.dart
+++ lib/features/review_management/views/review_history_screen.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gamelog/core/domain/entities/game.dart';
-import 'package:gamelog/features/review_management/views/review_screen.dart';
+import 'package:gamelog/features/review_management/models/retrieve_review_history_response.dart';
+import 'package:gamelog/features/review_management/providers/retrieve_review_history_controller.dart';
 
 import 'package:gamelog/l10n/app_localizations.dart';
+import 'package:gamelog/l10n/app_localizations_extension.dart';
 
-import '../../../core/domain/entities/player.dart';
+import '../../../core/domain/entities/game.dart';
 import '../../../core/domain/entities/review.dart';
+import '../../../core/domain/failures/failure.dart';
+import '../../../widgets/app_global_loader.dart';
 import '../../../widgets/app_icon_button.dart';
 import '../../../widgets/app_module_title.dart';
-import '../../../widgets/app_my_review_card.dart';
-import '../../../widgets/app_search_bar.dart';
+import '../../../widgets/app_poster_review_card.dart';
+import '../../auth/providers/auth_providers.dart';
+import '../providers/search_game_controller.dart';
+
+final searchResultProvider = StateProvider<List<Game?>>((ref) => []);
+
+final retrieveResultsProvider = StateProvider<List<Review>>((ref) => []);
 
 class ReviewHistoryScreen extends ConsumerStatefulWidget {
-  const ReviewHistoryScreen({super.key});
+  final int idPlayerToSearch;
+  const ReviewHistoryScreen({super.key, required this.idPlayerToSearch});
 
   @override
   ConsumerState<ReviewHistoryScreen> createState() =>
@@ -22,29 +31,35 @@ class ReviewHistoryScreen extends ConsumerStatefulWidget {
 }
 
 class _ReviewHistoryScreenState extends ConsumerState<ReviewHistoryScreen> {
-  List<Review> allGames = [
-
-  ];
-
-  List<Review> results = [];
   bool isLoading = false;
 
-  Future<void> _search(String query) async {
+  Future<void> _searchGame(String query) async {
     setState(() => isLoading = true);
 
-    // Simula un delay de API
-    await Future.delayed(const Duration(milliseconds: 600));
+    await ref.read(searchGameControllerProvider.notifier).searchGame(query);
 
-    // setState(() {
-    //   results = allGames
-    //       .where(
-    //         (review) =>
-    //             review.game.name.toLowerCase().contains(query.toLowerCase()),
-    //       )
-    //       .toList();
-    //
-    //   isLoading = false;
-    // });
+    setState(() => isLoading = false);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() => _search());
+  }
+
+  Future<void> _search() async {
+    if (!mounted) return;
+
+    setState(() => isLoading = true);
+
+    int idPlayer = ref.read(currentUserProvider.notifier).state!.idPlayer;
+
+    await ref
+        .read(retrieveReviewHistoryControllerProvider.notifier)
+        .retrieveReviewHistory(widget.idPlayerToSearch, idPlayer);
+
+    if (mounted) setState(() => isLoading = false);
   }
 
   @override
@@ -54,7 +69,85 @@ class _ReviewHistoryScreenState extends ConsumerState<ReviewHistoryScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final results = ref.watch(retrieveResultsProvider);
+
     final l10n = AppLocalizations.of(context)!;
+
+    ref.listen<AsyncValue<Game?>>(searchGameControllerProvider, (
+      previous,
+      next,
+    ) {
+      if (previous?.isLoading == true && next.isLoading == false) {
+        next.when(
+          loading: () {},
+          data: (response) {
+            ref.read(globalLoadingProvider.notifier).state = false;
+
+            if (response == null) return;
+
+            ref.read(searchResultProvider.notifier).state.add(response);
+          },
+          error: (error, stack) {
+            ref.read(globalLoadingProvider.notifier).state = false;
+
+            ref.read(searchResultProvider.notifier).state.add(null);
+
+            final msg = error is Failure
+                ? (error.serverMessage ?? l10n.byKey(error.code))
+                : error.toString();
+
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(msg)));
+            });
+          },
+        );
+      }
+
+      if (next.isLoading) {
+        ref.read(globalLoadingProvider.notifier).state = true;
+      }
+    });
+
+    ref.listen<AsyncValue<RetrieveReviewHistoryResponse?>>(
+      retrieveReviewHistoryControllerProvider,
+      (previous, next) {
+        if (previous?.isLoading == true && next.isLoading == false) {
+          next.when(
+            loading: () {},
+            data: (response) async {
+              ref.read(globalLoadingProvider.notifier).state = false;
+
+              if (response == null) return;
+              for (int i = 0; i < response.reviews.length; i++) {
+                await _searchGame(response.reviews[i].name);
+              }
+
+              ref.read(retrieveResultsProvider.notifier).state =
+                  response.reviews;
+            },
+            error: (error, stack) {
+              ref.read(globalLoadingProvider.notifier).state = false;
+
+              final msg = error is Failure
+                  ? (error.serverMessage ?? l10n.byKey(error.code))
+                  : error.toString();
+
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text(msg)));
+              });
+            },
+          );
+        }
+
+        if (next.isLoading) {
+          ref.read(globalLoadingProvider.notifier).state = true;
+        }
+      },
+    );
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -72,36 +165,38 @@ class _ReviewHistoryScreenState extends ConsumerState<ReviewHistoryScreen> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
-            children: <Widget>[
-              AppSearchBar(hint: l10n.search, onSearch: _search),
-
-              const SizedBox(height: 12),
-
-              if (isLoading)
-                const Expanded(
-                  child: Center(child: CircularProgressIndicator()),
-                )
-              else
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: results.length,
-                    itemBuilder: (_, i) {
-                      return AppMyReviewCard(
-                        date: DateTime.now(),
-                        name: 'd',//results[i].game.name,
-                        imageUrl: "",
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => ReviewScreen(review: results[i]),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: results.length,
+                  itemBuilder: (_, i) {
+                    return AppPosterReviewCard(
+                      date: DateTime.parse(results[i].date),
+                      opinion: results[i].opinion,
+                      imageUrl:
+                          ref
+                              .read(searchResultProvider.notifier)
+                              .state[i]
+                              ?.backgroundImage ??
+                          'https://picsum.photos/800/450',
+                      // rating: results[i].rating,
+                      // opinion: results[i].opinion,
+                      // onDelete: () {},
+                      // isLiked: results[i].isLiked,
+                      onTap: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (_) => ProfileScreen(),
+                        //   ),
+                        // );
+                      },
+                      name: results[i].name,
+                      rating: results[i].rating,
+                    );
+                  },
                 ),
+              ),
             ],
           ),
         ),
